4b26e673d46868a57173b24e7882038a82cedded
Your Name <you@example.com>
2023-01-15 00:38:29 +0300

    HW-1

---
+++ index.js
@@ -0,0 +1,41 @@
+// Task 1
+
+let x = 20;
+let y = 58;
+let z = 42;
+
+console.log(x+y+z);
+
+// Task 2
+
+let secMin = 60;
+let minHour = 60;
+let hourDay = 24;
+let dayYear = 365;
+
+let myAgeInSeconds = (dayYear * 24 * hourDay * minHour * secMin);
+
+console.log(myAgeInSeconds);
+
+// Task 3
+
+let count = 42;
+let userName = '42';
+
+let countTextQuotes = `${count}`;
+let countTextType = count.toString();
+
+console.log(countTextQuotes);
+console.log(countTextType);
+
+// Task 4
+
+let a = 1;
+let b = 2;
+let c = "белых медведей";
+
+let sum = `${a}` + `${b} ` + c;
+
+console.log(sum);
+
+// Task 5
